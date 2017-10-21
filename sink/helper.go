9e3cfd7f3b13df2fca269c4d11b5a2feaa7b1d9c
Andrei Burd <bandrei@yotpo.com>
2017-10-21 15:14:45 +0300

    Kafka Sink

--- sink/helper.go
+++ sink/helper.go
@@ -9,7 +9,7 @@ import (
 func GetSink() (Sink, error) {
 	sinkType := os.Getenv("SINK_TYPE")
 	if sinkType == "" {
-		return nil, fmt.Errorf("Missing SINK_TYPE: amqp, kinesis or stdout")
+		return nil, fmt.Errorf("Missing SINK_TYPE: amqp, kafka,kinesis or stdout")
 	}
 
 	switch sinkType {
@@ -17,11 +17,13 @@ func GetSink() (Sink, error) {
 		fallthrough
 	case "rabbitmq":
 		return NewRabbitmq()
+	case "kafka":
+		return NewKafka()
 	case "kinesis":
 		return NewKinesis()
 	case "stdout":
 		return NewStdout()
 	default:
-		return nil, fmt.Errorf("Invalid SINK_TYPE: amqp, kinesis or stdout")
+		return nil, fmt.Errorf("Invalid SINK_TYPE: %s, Valid values: amqp, kafka, kinesis or stdout",sinkType)
 	}
 }
