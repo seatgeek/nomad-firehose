cb06437abbc4495ae3172f819ed6012a2be11776
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2018-03-05 20:45:59 -0500

    fix: reorganize sinks

--- sink/helper.go
+++ sink/helper.go
@@ -9,25 +9,25 @@ import (
 func GetSink() (Sink, error) {
 	sinkType := os.Getenv("SINK_TYPE")
 	if sinkType == "" {
-		return nil, fmt.Errorf("Missing SINK_TYPE: amqp, kafka, kinesis, nsq, redis or stdout")
+		return nil, fmt.Errorf("Missing SINK_TYPE: amqp, kafka, kinesis, nsq, rabbitmq, redis or stdout")
 	}
 
 	switch sinkType {
 	case "amqp":
 		fallthrough
-	case "rabbitmq":
-		return NewRabbitmq()
 	case "kafka":
 		return NewKafka()
 	case "kinesis":
 		return NewKinesis()
-	case "stdout":
-		return NewStdout()
 	case "nsq":
 		return NewNSQ()
+	case "rabbitmq":
+		return NewRabbitmq()
 	case "redis":
 		return NewRedis()
+	case "stdout":
+		return NewStdout()
 	default:
-		return nil, fmt.Errorf("Invalid SINK_TYPE: %s, Valid values: amqp, kafka, kinesis, nsq, redis or stdout", sinkType)
+		return nil, fmt.Errorf("Invalid SINK_TYPE: %s, Valid values: amqp, kafka, kinesis, nsq, rabbitmq, redis or stdout", sinkType)
 	}
 }
